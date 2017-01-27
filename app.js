f1ecb6347fbc398e78a956ae078ced249752bcbe
Reyburn <dreyburn@sealimited.local>
2017-01-27 14:57:46 -0500

    angular rewrite

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ angular.module('portfolioSite', ['ngMaterial','ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/', {
     	templateUrl: 'pages/about.html',
+      requiresHTTPS: true
     }).when('/about', {
     	templateUrl: 'pages/about.html',
+      requiresHTTPS: true
     }).when('/portfolio', {
     	templateUrl: 'pages/portfolio.html',
+      requiresHTTPS: true
     }).when('/resume', {
     	templateUrl: 'pages/resume.html',
+      requiresHTTPS: true
     }).when('testimonials', {
     	templateUrl: 'pages/testimonials.html',
+      requiresHTTPS: true
     })
 }])
 
